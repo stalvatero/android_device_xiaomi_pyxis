bb4de3f5b140cd3ed89322167c4ca07f4bc118d5
Joey Huab <joey@evolution-x.org>
2020-07-07 05:03:03 +0000

    pyxis: Update to Coral's July fingerprint
    Signed-off-by: DarkAngelGR <asavvo01@gmail.com>

--- init/init_pyxis.cpp
+++ init/init_pyxis.cpp
@@ -74,5 +74,5 @@ void vendor_load_properties()
 	property_override("ro.oem_unlock_supported", "0");
 	property_override("ro.apex.updatable", "true");
 	property_override("ro.control_privapp_permissions", "log");
-	property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200605.001/6392402:user/release-keys");
+	property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ3A.200705.002/6506677:user/release-keys");
 }
