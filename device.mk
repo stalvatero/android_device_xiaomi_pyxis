c852303bf03cf4eeeadf2dfd62da68774458aac1
stalvatero <soscurato@gmail.com>
2020-07-17 13:15:23 +0200

    Add XiaomiParts

--- device.mk
+++ device.mk
@@ -490,6 +490,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# XiaomiParts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
