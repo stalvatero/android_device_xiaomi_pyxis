b453f25a69df545e0fdd3bb41246ea6068f80478
stalvatero <64108632+stalvatero@users.noreply.github.com>
2020-07-20 00:52:38 +0200

    try to fix fod

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,8 @@ USE_DEVICE_SPECIFIC_GPS := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
+
 
 # NFC
 TARGET_USES_NQ_NFC := true
