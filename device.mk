0a29e3bc7bcd436f52dc5baad0be7c672d0cfc6a
stalvatero <soscurato@gmail.com>
2020-07-21 17:28:43 +0200

    Pyxis: Audio config

--- device.mk
+++ device.mk
@@ -216,6 +216,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@5.0-impl \
+    android.hardware.audio.common@2.0-util \
+    android.hardware.audio.common@5.0-util \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
