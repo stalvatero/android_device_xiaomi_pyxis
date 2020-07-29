0858f97ed3bc740bbe9b5e6dc3a9b96d9e4c6c2e
stalvatero <soscurato@gmail.com>
2020-07-29 16:00:04 +0200

    Update BootCompletedReceiver.java

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -32,7 +32,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
-        DiracUtils.initialize(context);
         DozeUtils.checkDozeService(context);
     }
 }
