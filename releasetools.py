62ba1664add0dd2b1f35bd0e0319fa2eeaacff59
stalvatero <soscurato@gmail.com>
2020-07-30 17:12:29 +0200

    Update releasetools.py

--- releasetools.py
+++ releasetools.py
@@ -9,37 +9,40 @@ import common
 import re
 
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  OTA_InstallEnd(info, info.input_zip)
   return
 
 def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  OTA_InstallEnd(info, info.target_zip)
   return
 
 def FullOTA_Assertions(info):
   AddModemAssertion(info, info.input_zip)
+  AddVendorAssertion(info, info.input_zip)
   return
 
 def IncrementalOTA_Assertions(info):
   AddModemAssertion(info, info.target_zip)
+  AddVendorAssertion(info, info.target_zip)
   return
 
-def AddImage(info, basename, dest):
+def AddImage(info, input_zip, basename, dest):
   path = "IMAGES/" + basename
-  if path not in info.input_zip.namelist():
+  if path not in input_zip.namelist():
     return
 
-  data = info.input_zip.read(path)
+  data = input_zip.read(path)
   common.ZipWriteStr(info.output_zip, basename, data)
-  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
-def OTA_InstallEnd(info):
-  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+
+def OTA_InstallEnd(info, input_zip):
+  info.script.Print("Patching firmware images...")
+  AddImage(info, input_zip, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   return
 
 def AddModemAssertion(info, input_zip):
-  android_info = info.input_zip.read("OTA/android-info.txt")
+  android_info = input_zip.read("OTA/android-info.txt")
   m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
   miui_version = re.search(r'require\s+version-miui\s*=\s*(.+)', android_info)
   if m and miui_version:
@@ -49,4 +52,15 @@ def AddModemAssertion(info, input_zip):
         (len(firmware_version) and '*' not in firmware_version)):
       cmd = 'assert(xiaomi.verify_modem("{}") == "1" || abort("ERROR: This package requires firmware from MIUI {} developer build or newer. Please upgrade firmware and retry!"););'
       info.script.AppendExtra(cmd.format(timestamp, firmware_version))
+  return
+
+def AddVendorAssertion(info, input_zip):
+  android_info = input_zip.read("OTA/android-info.txt")
+  v = re.search(r'require\s+version-vendor\s*=\s*(.+)', android_info)
+  miui_version = re.search(r'require\s+version-miui\s*=\s*(.+)', android_info)
+  if v and miui_version:
+    build_date_utc, vndk_version = v.group(1).rstrip().split(',')
+    firmware_version = miui_version.group(1).rstrip()
+    cmd = 'assert(xiaomi.verify_vendor("{}", "{}") == "1" || abort("ERROR: This package requires vendor from MIUI {} developer build or newer. Please upgrade vendor image along with matching firmware and retry!"););'
+    info.script.AppendExtra(cmd.format(build_date_utc, vndk_version, firmware_version))
   return
