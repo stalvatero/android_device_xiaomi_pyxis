37aa7465860b5ff40a01e6013bdacac0928a76e4
stalvatero <soscurato@gmail.com>
2020-10-14 12:11:19 +0200

    pyxis: Add overlay for masking the notch
    Go to Settings > Developer Options > Simulate display with cutout > "No cutout". Cutout should be hidden. Rotate screen, take screenshots, screenrecord should all work.

--- device.mk
+++ device.mk
@@ -23,6 +23,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-pe
 
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 # Boot animation
